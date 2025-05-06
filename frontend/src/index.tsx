9946800657d3688a756a949fc7e3957ab7331d89
yuta.sano <snyt45@gmail.com>
2025-05-06 19:35:22 +0900

    providersに変更

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HelloProvider } from "./provider/HelloProvider";
+import { HelloProvider } from "./providers/HelloProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
