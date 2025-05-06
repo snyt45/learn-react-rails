346540941e23a645507b85df7a5455aa3ebf0a0d
yuta.sano <snyt45@gmail.com>
2025-05-07 00:02:23 +0900

    ユーザー登録するコントローラーをオーバーライド

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   namespace :api do
     namespace :v1 do
