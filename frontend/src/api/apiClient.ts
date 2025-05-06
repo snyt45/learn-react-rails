b8053ec952735e52d8a6763daee1abd1509526cf
yuta.sano <snyt45@gmail.com>
2025-05-07 00:13:38 +0900

    dataフィールドを追加

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -3,6 +3,7 @@ import axios, { AxiosResponse } from "axios";
 interface ApiRequestOptions {
   method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   url: string;
+  data?: any;
   params?: any;
 }
 
@@ -11,7 +12,7 @@ const API_BASE_URL = process.env.REACT_APP_API_URL;
 const apiClient = async (
   options: ApiRequestOptions
 ): Promise<AxiosResponse> => {
-  const { method, url, params } = options;
+  const { method, url, data, params } = options;
 
   const client = axios.create({
     baseURL: API_BASE_URL,
@@ -28,16 +29,16 @@ const apiClient = async (
         respose = await client.get(url, { params });
         break;
       case "POST":
-        respose = await client.post(url, params);
+        respose = await client.post(url, data);
         break;
       case "PUT":
-        respose = await client.put(url, params);
+        respose = await client.put(url, data);
         break;
       case "PATCH":
-        respose = await client.patch(url, params);
+        respose = await client.patch(url, data);
         break;
       case "DELETE":
-        respose = await client.delete(url, { data: params });
+        respose = await client.delete(url, { data, params });
         break;
       default:
         throw new Error("Invalid HTTP method");
